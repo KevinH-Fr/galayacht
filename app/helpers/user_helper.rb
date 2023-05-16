61dff92cc3a17f6e9ee93a855114c20134801bf3
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-05-16 19:02:48 +0200

    cards sur pages log

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,9 +1,9 @@
 module UserHelper
     def user_preneur
-        Preneur.where(user_id: current_user.id).first
+        Preneur.where(user_id: current_user.id).first if  Preneur.where(user_id: current_user).present?
     end
     def user_bailleur
-        Bailleur.where(user_id: current_user.id).first
+        Bailleur.where(user_id: current_user.id).first if Bailleur.where(user_id: current_user).present?
     end
 
 end
