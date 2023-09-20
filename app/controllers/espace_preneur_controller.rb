0837d3df3566b170c3b710422b138c7355da1239
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-09-20 21:20:57 +0200

    form recherche espace preneur - cacher footer - format boutons

--- app/controllers/espace_preneur_controller.rb
+++ app/controllers/espace_preneur_controller.rb
@@ -1,6 +1,20 @@
 class EspacePreneurController < ApplicationController
   def index
-    @produits = Produit.actif.limit(6)
     @destinations = Destination.all
+
+    @q = Produit.actif.ransack(params[:q])
+
+    if params[:city].present?
+      destination = params[:city].titleize
+      @produits = Produit.actif.where(destination_id: destination)
+      @pagy, @produits = pagy(@produits, items: 5)
+
+    else
+      
+      @q = Produit.actif.ransack(params[:q])
+      @produits = @q.result(distinct: true).order(created_at: :desc)
+      @pagy, @produits = pagy(@produits, items: 5)
+    end
+
   end
 end
