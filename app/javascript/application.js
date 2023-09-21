43602f155844124d01014beec1b4fa2deefd41b9
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-09-21 19:03:15 +0200

    require tree js - apple icon - more content home page explications - css compressor

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,9 @@ import "controllers"
 import "@hotwired/turbo-rails"
 
 //= require range-slider
+
+//= require chart.umd.min
+//= require tui-time-picker
+//= require tui-date-picker
+//= require toastui-calendar.min
+//= require bootstrap.bundle.min
