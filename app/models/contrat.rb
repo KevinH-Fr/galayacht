b67b849ffafa00554bd9ac9d43d3b12d3c657fe1
KevinH-Fr <kevin.hoffman.france@gmail.com>
2023-10-22 19:59:07 +0200

    ajout des differents collapses

--- app/models/contrat.rb
+++ app/models/contrat.rb
@@ -1,3 +1,7 @@
 class Contrat < ApplicationRecord
     belongs_to :reservation, optional: true
+
+    def full_name
+        "ref_#{id} #{reservation.id} #{bateau_proprietaire_nom} #{loc_nom} "
+    end
 end
